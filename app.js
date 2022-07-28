1fd65a17bdcb7f251e3ff0e9cf36fbdb07ac4289
gfunk77 <faraci77@gmail.com>
2022-07-27 22:25:09 -0400

    rgb flipper

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
 "use strict";
+
+// select elements
+const title = document.querySelector(".title-color");
+const backgroundText = document.querySelector(".title");
+const main = document.querySelector("main");
+const value = document.querySelector(".value");
+const btn = document.querySelector(".btn");
+
+function randomColor(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+function changeColor() {
+  value.style.color = main.style.backgroundColor;
+  title.style.color = main.style.backgroundColor;
+  value.textContent = main.style.backgroundColor;
+  backgroundText.style.color = main.style.backgroundColor;
+}
+
+btn.addEventListener("click", function () {
+  main.style.backgroundColor = `rgb(${randomColor(0, 255)},${randomColor(
+    0,
+    255
+  )}, ${randomColor(0, 255)})`;
+  changeColor();
+});
